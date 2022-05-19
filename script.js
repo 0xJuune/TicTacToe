50448cbb978364e522a753cc597f143fae05f6b4
Jonathan <jlbenfield.cccmc@gmail.com>
2022-05-19 18:04:11 -0400

    add basic player swap function

--- script.js
+++ script.js
@@ -1,32 +1,51 @@
+let board2 = new Array(9);
 const gameBoard = (() => {
     let board = new Array(9);
-    const show = console.log(board);
-    const cell = document.querySelectorAll('.cell')
+    const cell = document.querySelectorAll('.cell');
 
     let currentPlayerSign = 'X' // should probably replace this with player.currentplayer or something weird
 
 
     const claimCell = (a) => {
         if (a.textContent === '') {
-            a.textContent = currentPlayerSign
-            board[a.id] = currentPlayerSign
+            a.textContent = currentPlayerSign;
+            board[a.id] = currentPlayerSign;
         }
         else {
-            
+            console.log('cell previously claimed');
         }
     }
 
+    const swapPlayer = (a) => {
+        console.log('poop')
+        switch(a) {
+            case "X":
+                currentPlayerSign = 'O';
+                console.log("CASE X");
+                break;
+            case "O":
+                currentPlayerSign = 'X';
+                console.log("CASE O");
+            break;
+        }
+    };
+
+
+
     cell.forEach((position) => {
         // position.addEventListener('click', () => console.log(position.id)) 
-        position.addEventListener('click', () => claimCell(position)) 
+        position.addEventListener('click', () => {
+            claimCell(position);
+            swapPlayer(currentPlayerSign);
+        }) 
     }); 
 
     return {
-        show,
+
     };
   })();
 
-const player = (symbol, playerType) => {
+const player = (symbol, playerType, currentPlayer) => {
     // let team = symbol;
     let score = 0;
     // const getTeam = () => {return team};
@@ -37,6 +56,7 @@ const player = (symbol, playerType) => {
     return {
         symbol,
         playerType,
+        currentPlayer,
         getScore
     }
 }
